b3d40e360c2365755fd52f17985ad5fd9145b53c
Alessandro <alessandrofrancescocastelli@outlook.it>
2024-08-20 16:25:05 +0200

    Added About Page

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -3,8 +3,36 @@ import styles from "./about.module.css"
 
 function AboutPage(){
     return (
-        <div className={styles.imgContainer}>
-            <Image src="/about.png" alt="about" width={300} height={300}/>    
+        <div className={styles.container}>
+            <div className={styles.textContainer}>
+                <h2 className={styles.subtitle}>About Agency</h2>
+                <h1 className={styles.title}>
+                    We create digital ideas that are bigger, bolder, braver and better.
+                </h1>
+                <p className={styles.desc}>
+                    We create digital ideas that are bigger, bolder, braver and better. We
+                    believe in good ideas flexibility and precission We’re world’s Our
+                    Special Team best consulting & finance solution provider. Wide range
+                    of web and software development services.
+                </p>
+                <div className={styles.boxes}>
+                    <div className={styles.box}>
+                        <h2>10 K+</h2>
+                        <p>Year of experience</p>
+                    </div>
+                    <div className={styles.box}>
+                        <h2>10 K+</h2>
+                        <p>Year of experience</p>
+                    </div>
+                    <div className={styles.box}>
+                        <h2>10 K+</h2>
+                        <p>Year of experience</p>
+                </div>
+            </div>
+            </div>
+            <div className={styles.imgContainer}>
+                <Image src="/about.png" alt="about" fill className={styles.img}/>    
+            </div>
         </div>
     )
 }
